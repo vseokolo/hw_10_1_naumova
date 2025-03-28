1ef8fc36fd4afcf06b064699382de99b85eedd40
Anastasiia Naumova <vsedaleko@gmail.com>
2025-03-28 12:13:12 +0300

    init (1) создали проект заново по подобию предыдущего

---
+++ src/mask.py
@@ -0,0 +1,60 @@
+from typing import Union
+
+'''аннотируем тип переменной'''
+
+def get_mask_card_number(сard_number: Union[str]) -> str:
+    '''маскируем номер банковской карты'''
+
+    card_number_spaceless = сard_number.replace(' ', '')
+    '''убираем возможные пробелы'''
+
+    if len(card_number_spaceless) != 16:
+        return 'Номер карты должен содержать 16 цифр.'
+
+    if not card_number_spaceless.isdigit():
+        return 'Номер карты должен содержать только цифры.'
+
+    first_part_card = card_number_spaceless[:4]
+    second_part_card = card_number_spaceless[4:6]
+    third_part_card = '**'
+    fourth_part_card = '****'
+    fifth_part_card = card_number_spaceless[12:16]
+
+    '''определяем, какие части карты будут замаскированы'''
+
+    masked_number_card = (f'{first_part_card} {second_part_card}{third_part_card} '
+                     f'{fourth_part_card} {fifth_part_card}')
+
+    '''складываем части карты вместе'''
+
+    return masked_number_card
+
+
+print(get_mask_card_number('7000792289606361'))
+
+
+def get_mask_account(account_number: Union[str]) -> str:
+    '''маскируем номер банковского счета'''
+
+    account_number_spaceless = account_number.replace(' ', '')
+    '''убираем возможные пробелы'''
+
+    if len(account_number_spaceless) != 20:
+        return 'Номер аккаунта должен содержать 20 цифр.'
+
+    if not account_number_spaceless.isdigit():
+        return 'Номер аккаунта должен содержать только цифры.'
+
+    first_part_account = '**'
+    second_part_account = account_number_spaceless[-4:]
+
+    '''определяем, какие части аккаунта будут замаскированы'''
+
+    masked_number_account = (f'{first_part_account} {second_part_account}')
+
+    '''складываем части аккаунта вместе'''
+
+    return masked_number_account
+
+
+print(get_mask_account('73654108430135874305'))
---
+++ src/widget.py
@@ -0,0 +1,52 @@
+from typing import Union
+
+"""аннотируем тип переменной"""
+
+from datetime import datetime
+
+"""импортируем модуль datetime"""
+
+
+def mask_account_card(card_info: Union[str]) -> str:
+    """Создаём функцию, принимающая информацию о карте в виде строки"""
+
+    if "Счет" in card_info:
+        first_part_account = "**"
+        second_part_account = card_info[-4:]
+        """определяем, какие части аккаунта будут замаскированы"""
+        card_info = f"Счет {first_part_account}{second_part_account}"
+        """складываем части аккаунта вместе"""
+        return card_info
+
+    else:
+        first_part_card = card_info[:-12]
+        second_part_card = card_info[-12:-10]
+        third_part_card = "**"
+        fourth_part_card = "****"
+        fifth_part_card = card_info[-4:]
+
+        """определяем, какие части карты будут замаскированы"""
+
+        card_info = f"{first_part_card} {second_part_card}{third_part_card} " f"{fourth_part_card} {fifth_part_card}"
+
+        """складываем части карты вместе"""
+
+        return card_info
+
+
+print(mask_account_card("MasterCard 7158300734726758"))
+
+
+def get_date(user_date):
+    """определяем функцию для получения даты"""
+
+    user_year = datetime.strptime(user_date[:10], "%Y-%m-%d").year
+    user_month = datetime.strptime(user_date[:10], "%Y-%m-%d").month
+    user_day = datetime.strptime(user_date[:10], "%Y-%m-%d").day
+    """получаем год, месяц и день из строки пользователя"""
+
+    return f"{user_day:02}.{user_month:02}.{user_year}"
+    """возвращаем отформатированную дату"""
+
+
+print(get_date("2024-03-11T02:26:18.671407"))
