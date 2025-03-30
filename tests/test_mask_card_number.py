1af5db14fadc821b1a0612e22cd0c23357f7ca92
Anastasiia Naumova <vsedaleko@gmail.com>
2025-03-31 01:09:11 +0300

    (3) в пакете с тестами создала файл test_mask_card_number.py, протестировав в нем ф-ию get_mask_card_number и с помощью параметризации и также вызвав возможные ошибки

---
+++ tests/test_mask_card_number.py
@@ -0,0 +1,25 @@
+from src.mask import get_mask_card_number
+
+import pytest
+
+@pytest.mark.parametrize('card_number, expected',
+                         [
+                             ('7000792289606361', '7000 79** **** 6361'),
+                             ('8429482403109324', '8429 48** **** 9324'),
+                             ('0000000100001001', '0000 00** **** 1001'),
+                             ('0000066666666666', '0000 06** **** 6666')
+                         ])
+
+def test_get_mask_card_number(card_number, expected):
+    assert get_mask_card_number(card_number) == expected
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number('111')
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_card_number('aaa')
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_card_number('aaaa1111бббm---ß')
+
+
