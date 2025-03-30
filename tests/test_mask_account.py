893b790c3b7c9ac9cf8b1acae264cef4acf3e33c
Anastasiia Naumova <vsedaleko@gmail.com>
2025-03-31 01:10:36 +0300

    (4) в пакете с тестами создала файл test_mask_account.py, протестировав в нем ф-ию get_mask_account и с помощью параметризации и также вызвав возможные ошибки

---
+++ tests/test_mask_account.py
@@ -0,0 +1,22 @@
+from src.mask import get_mask_account
+
+import pytest
+
+@pytest.mark.parametrize('account_number, expected',
+                         [
+                             ('73654108430135874305', '** 4305'),
+                             ('49248242942040240130', '** 0130'),
+                             ('00000000000000000000', '** 0000'),
+                             ('10101010100222222222', '** 2222')
+                         ])
+def test_get_mask_account(account_number, expected):
+    assert get_mask_account(account_number) == expected
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account('111')
+
+    with pytest.raises(ValueError) as exc_info:
+        get_mask_account('aaa')
+
+    with pytest.raises(TypeError) as exc_info:
+        get_mask_account('aaaa1111бббm---ß,,,p')
