c05e2e8c274eccf48333e0e2489296ec6ded3242
Anastasiia Naumova <vsedaleko@gmail.com>
2025-04-02 02:50:33 +0300

    (8) добавила и протестировала возможные ошибки в  test_processing_state.py

--- tests/test_processing_state.py
+++ tests/test_processing_state.py
@@ -1,6 +1,5 @@
 from typing import Dict, List
 from src.processing import filter_by_state, operations_data, operations_data_sorted
-
 import pytest
 
 @pytest.mark.parametrize('state, expected',
@@ -8,7 +7,16 @@ import pytest
                              (operations_data, operations_data_sorted)
                          ])
 
-
 def test_filter_by_state(state: str, expected: List[Dict]):
+    """используем параматризацию для проверки сразу нескольких тестов"""
     assert filter_by_state(state) == expected
 
+with pytest.raises(TypeError) as exc_info:
+
+    test_filter_by_state('20-03-11T02:26:18.671407')
+
+with pytest.raises(TypeError) as exc_info:
+    test_filter_by_state('01-2000-01T09:56:09.000430')
+
+with pytest.raises(TypeError) as exc_info:
+    test_filter_by_state('')
